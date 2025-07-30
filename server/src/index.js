20a64c874df09b486357e9ad890a578d70bbf51d
Adit <adit.dashore2004@gmail.com>
2025-07-31 02:45:12 +0530

    clean up imports in index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,9 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import path from "path";
 import { fileURLToPath } from "url"; 
 import { app } from "./app.js";
 import path from 'path';
-import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
